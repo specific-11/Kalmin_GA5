1c5a098178a5e78d9b3ea59e9f348682fbcc9fcc
specific-11 <rollkadzuha@gmail.com>
2025-11-17 13:07:12 +0300

    Update Zadanie5.java
    Added code

--- Kalmin_GA5-master/src/KalminGA_Razdel_2/Zadanie5.java
+++ Kalmin_GA5-master/src/KalminGA_Razdel_2/Zadanie5.java
@@ -14,8 +14,29 @@ public class Zadanie5 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
-}
+public static void main(String[] args) {
+
+        int[][] a5 = {
+            {1, 2, 3, 4},
+            {5, 0, 7, 8},
+            {9, 8, 7, 6},
+            {0, 3, 4, 5},
+            {1, 2, 6, 4}
+        };
+
+        for (int i5 = 0; i5 < a5.length; i5++) {
+            
+            boolean FindZero5 = false;
+            for (int j = 0; j < a5[i5].length; j++) {
+                if (a5[i5][j] == 0) {
+                    FindZero5 = true;
+                }
+            }
+            if (FindZero5 == true) {
+                for (int j = 0; j < a5[i5].length; j++) {
+                    System.out.print(a5[i5][j] + " ");
+                }
+                System.out.println(); 
+            }
+        }
+}}
