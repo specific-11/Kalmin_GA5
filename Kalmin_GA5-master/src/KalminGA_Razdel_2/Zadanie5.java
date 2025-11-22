8b888b093e1383e1e9a6710360177e947c91e3eb
specific-11 <rollkadzuha@gmail.com>
2025-11-22 11:30:08 +0300

    Update Zadanie5.java

--- Kalmin_GA5-master/src/KalminGA_Razdel_2/Zadanie5.java
+++ Kalmin_GA5-master/src/KalminGA_Razdel_2/Zadanie5.java
@@ -2,7 +2,7 @@
  * Добавить вывод исходной таблицы
  * добавить math random (от 0 до 5) в таблицу, вместо конкретных чисел
  * выбирать там ноль (оставить тот же метод в поиске нуля)
- * 
+ * 12345
  */
 package KalminGA_Razdel_2;
 
@@ -15,15 +15,26 @@ public class Zadanie5 {
     /**
      * @param args the command line arguments
      */
-public static void main(String[] args) {
-
-        int[][] a5 = {
-            {1, 2, 3, 4},
-            {5, 0, 7, 8},
-            {9, 8, 7, 6},
-            {0, 3, 4, 5},
-            {1, 2, 6, 4}
-        };
+    public static void main(String[] args) {
+        System.out.println("Из данной прямоугольной таблицы вывести на экран строки, содержащие\n" +
+"хотя бы один нулевой элемент.");
+        int[][] a5 = new int[5][4];
+        
+        for (int i = 0; i < a5.length; i++) {
+            for (int j = 0; j < a5[i].length; j++) {
+                a5[i][j] = (int)(Math.random() * 6); 
+            }
+        }
+        
+        System.out.println("Исходная таблица:");
+        for (int i = 0; i < a5.length; i++) {
+            for (int j = 0; j < a5[i].length; j++) {
+                System.out.print(a5[i][j] + " ");
+            }
+            System.out.println();
+        }
+        
+        System.out.println("\nСтроки, содержащие нули:");
 
         for (int i5 = 0; i5 < a5.length; i5++) {
             
@@ -40,6 +51,5 @@ public static void main(String[] args) {
                 System.out.println(); 
             }
         }
-}}
-
-
+    }
+}
