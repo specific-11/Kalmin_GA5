a7b04b768b6d062dc3ce91ee3c1d3b495da4384f
specific-11 <rollkadzuha@gmail.com>
2025-11-22 10:51:00 +0300

    Update Zadanie5.java

--- Kalmin_GA5-master/src/KalminGA_Razdel_2/Zadanie5.java
+++ Kalmin_GA5-master/src/KalminGA_Razdel_2/Zadanie5.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Добавить вывод исходной таблицы
+ * добавить math random 
+ * выбирать там ноль
  */
 package KalminGA_Razdel_2;
 
@@ -40,3 +40,4 @@ public static void main(String[] args) {
             }
         }
 }}
+
