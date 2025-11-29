6b2ea41ddec4b42f922ec1786a62af45985593bc
specific-11 <rollkadzuha@gmail.com>
2025-11-29 12:27:41 +0300

    Update Zadanie30.java

--- Kalmin_GA5-master/src/KalminGA_Razdel_3/Zadanie30.java
+++ Kalmin_GA5-master/src/KalminGA_Razdel_3/Zadanie30.java
@@ -1,21 +1,30 @@
 /*
  * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
+ * НЕДОДЕЛАНО
  * and open the template in the editor.
  */
 package KalminGA_Razdel_3;
-
+import java.util.Scanner;
 /**
  *
  * @author student
  */
 public class Zadanie30 {
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+        System.out.println("Введенную строку вывести, расположив в каждой подстроке по 5 цифр.");
+          Scanner sc5 = new Scanner(System.in);
+       System.out.println("Введите числа:");
+       String counterWord5 = sc5.nextLine();
+       System.out.println("Спасибо! Вы ввели число " + counterWord5);
+      System.out.println(counterWord5.substring(1));
+      System.out.println(counterWord5.substring(2));
+      System.out.println(counterWord5.substring(3));
+      System.out.println(counterWord5.substring(4));
+      System.out.println(counterWord5.substring(5));
+      System.out.println(counterWord5.substring(6));
+       sc5.close();
+   }
 }
