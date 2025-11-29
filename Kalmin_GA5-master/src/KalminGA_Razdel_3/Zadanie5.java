b4b294c4dd57ef3e5e35ba3dc301984121adb1e5
specific-11 <rollkadzuha@gmail.com>
2025-11-29 12:27:13 +0300

    Update Zadanie5.java

--- Kalmin_GA5-master/src/KalminGA_Razdel_3/Zadanie5.java
+++ Kalmin_GA5-master/src/KalminGA_Razdel_3/Zadanie5.java
@@ -4,18 +4,30 @@
  * and open the template in the editor.
  */
 package KalminGA_Razdel_3;
-
+import java.util.Scanner;
 /**
  *
  * @author student
  */
 public class Zadanie5 {
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+    System.out.println("Сколько раз в тексте встречается заданное слово (слова разделены пробелами).");
+    Scanner sc5 = new Scanner(System.in); 
+    System.out.println("Введите Предложение, содержащее friend N кол-во раз:"); 
+    String counterWord = sc5.nextLine();
     
+    String[] words5 = counterWord.split(" ");
+    int count5 = 0;
+    
+    for (String word5:words5) {
+        if (word5.equals("friend")) {
+            count5++;
+        }
+    }
+     sc5.close();
+    System.out.println("Слово 'friend' встречается " + count5 + " раз(а)");
+    }
 }
